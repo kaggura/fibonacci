1e61c32216ae8259d3f278693775b9d93efa046c
dano <taicap24@icloud.com>
2022-02-24 12:57:07 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+// "use strict";
+// const memo =new Map();
+// memo.set(0,0);
+// memo.set(1,1);
+// function fib(n) {
+//   if (memo.has(n))  {
+//     return memo.get(n);
+//   }
+//   const value =fib(n-1)+fib(n-2);
+//   memo.set(n,value);
+//   return value;
+// }
+// const length =40;
+// for (let i=0;i<=length;i++){
+//   console.log(fib(i));
+// }
+
 "use strict";
 const memo =new Map();
 memo.set(0,0);
 memo.set(1,1);
-function fib(n) {
+memo.set(2,2);
+function tor(n) {
   if (memo.has(n))  {
     return memo.get(n);
   }
-  const vakue =fib(n-1)+fib(n-2);
+  const value =fib(n-1)+fib(n-2);
   memo.set(n,value);
   return value;
 }
